15dcb6a8be90b2b6b130add9643ad6be419e5e14
Miranda Swenson <glasspixie@gmail.com>
2023-09-29 18:54:44 -0700

    before tailwind

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <>
+        <input type='test' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+        }} />
+          <Employee name="Caleb" role="Intern"/>
+          <Employee name="Abby" role={role} />
+          <Employee name="John"/>
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
